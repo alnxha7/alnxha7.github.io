e6363128fe1bb5159fa2a5704cbb011026692f2c
[alansha m] <[alns4rha@gmail.com]>
2024-06-17 16:02:04 +0530

    1.0

--- public_html/assets/js/meyawo.js
+++ public_html/assets/js/meyawo.js
@@ -36,3 +36,22 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+document.getElementById('cv-download-btn').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = "public_html/doc/ALANSHA_RESUME.pdf";
+    link.download = 'ALANSHA_RESUME.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+document.getElementById('github-btn').addEventListener('click', function() {
+    window.open('https://github.com/alnxha7', '_blank');
+});
+
+document.getElementById('email-btn').addEventListener('click', function() {
+    const recipient = 'alansha71011@gmail.com';
+    const subject = 'Invitation to Interview for';
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${recipient}&su=${encodeURIComponent(subject)}`, '_blank');
+});
